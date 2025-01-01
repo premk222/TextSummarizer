91ec430e30e581c8c43a721d532218248c1f134e
premk222 <premkoyilada222@gmail.com>
2025-01-01 19:49:16 +0530

    data ingestion completed

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from src.textSummarizer.logging import logger
+from src.textSummarizer.pipeline.stage_1_data_ingestion_pipeline import DataIngestionTrainingPipeline
+# from src.textSummarizer.pipeline.stage_2_data_transformation_pipeline import DataTransformationTrainingPipeline
+# from src.textSummarizer.pipeline.stage_3_model_trainer_pipeline import ModelTrainerTrainingPipeline
 
-logger.info("logging is implemented")
+# from src.textSummarizer.pipeline.stage_4_model_evaluation import ModelEvaluationTrainingPipeline
+STAGE_NAME="Data Ingestion stage"
+
+try:
+    logger.info(f"stage {STAGE_NAME} initiated")
+    data_ingestion_pipeline=DataIngestionTrainingPipeline()
+    data_ingestion_pipeline.initiate_data_ingestion()
+    logger.info(f"Stage {STAGE_NAME} Completed")
+except Exception as e:
+    logger.exception(e)
+    raise e
