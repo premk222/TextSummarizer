37a2bd6646cc653802e17f8dfa1ec1b3ed88c807
premk222 <premkoyilada222@gmail.com>
2025-01-01 20:57:51 +0530

    project completed

--- template.py
+++ template.py
@@ -2,14 +2,6 @@ import os
 from pathlib import Path
 import logging
 
-logging.basicConfig(level=logging.INFO,format = '[%(asctime)s] : %(message)s:')
-
-project_name = "textSummarizer"
-
-import os
-from pathlib import Path
-import logging
-
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 
